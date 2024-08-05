b77cb30d2396628aaaa3914047434b5ada372562
sherif Abdelrhman <sellkady038@gmail.com>
2024-08-05 19:39:14 +0300

    add model classes for cities data.

---
+++ app/src/main/java/com/route/cities/models/Cities.kt
@@ -0,0 +1,8 @@
+package com.route.cities.models
+
+data class Cities(
+	val country: String? = null,
+	val coord: Coord? = null,
+	val name: String? = null,
+	val id: Int? = null
+)
---
+++ app/src/main/java/com/route/cities/models/Coord.kt
@@ -0,0 +1,6 @@
+package com.route.cities.models
+
+data class Coord(
+	val lon: Any? = null,
+	val lat: Any? = null
+)
