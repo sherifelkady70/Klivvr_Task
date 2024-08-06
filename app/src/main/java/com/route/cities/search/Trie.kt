8150a1054fd0ea55e3725db0119fa5d9820673c5
sherif Abdelrhman <sellkady038@gmail.com>
2024-08-06 17:27:33 +0300

    add search and findWords methods in Trie class.

--- app/src/main/java/com/route/cities/search/Trie.kt
+++ app/src/main/java/com/route/cities/search/Trie.kt
@@ -12,6 +12,23 @@ class Trie {
         node.isEndOfWord = true
     }
 
+    fun search(prefix: String): List<String> {
+        val results = mutableListOf<String>()
+        var node = root
+        for (char in prefix) {
+            node = node.children[char] ?: return emptyList()
+        }
+        findWords(node, prefix, results)
+        return results
+    }
 
+    private fun findWords(node: TrieNode, prefix: String, results: MutableList<String>) {
+        if (node.isEndOfWord) {
+            results.add(prefix)
+        }
+        for ((char, child) in node.children) {
+            findWords(child, prefix + char, results)
+        }
+    }
 
 }
