5892a5ecd62ec978b89bee3e37f2ae05b9875b50
sherif Abdelrhman <sellkady038@gmail.com>
2024-08-07 19:01:06 +0300

    make getCitiesForPage to get the data from entire list by page size (20) .

--- app/src/main/java/com/route/cities/data/data_manager/CitiesDataManagerImpl.kt
+++ app/src/main/java/com/route/cities/data/data_manager/CitiesDataManagerImpl.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.res.AssetManager
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.route.cities.data.PaginatedData
 import com.route.cities.data.contract.CitiesDataManagerInterface
 import com.route.cities.data.models.City
 import kotlinx.coroutines.Dispatchers
@@ -15,15 +16,25 @@ import javax.inject.Inject
 class CitiesDataManagerImpl @Inject constructor(
     private val context: Context
 ) : CitiesDataManagerInterface {
-
+    private lateinit var allCities: List<City>
+    private var pageSize = 20
     override suspend fun getCities(): List<City> {
-        return withContext(Dispatchers.IO) {
+         withContext(Dispatchers.IO) {
             val assetManager: AssetManager = context.assets
             val inputStream = assetManager.open("cities.json")
             val bufferReader = BufferedReader(InputStreamReader(inputStream))
             val jsonString = bufferReader.use { it.readText() }
             val cityTypeList = object : TypeToken<List<City>>() {}.type
-            Gson().fromJson(jsonString, cityTypeList)
+             allCities = Gson().fromJson(jsonString, cityTypeList)
         }
+        return allCities
+    }
+
+    fun getCitiesForPage(page: Int): PaginatedData<City> {
+        val startIndex = (page - 1) * pageSize
+        val endIndex = minOf(startIndex + pageSize, allCities.size)
+        val pageData = allCities.subList(startIndex, endIndex)
+        val totalPages = (allCities.size + pageSize - 1) / pageSize
+        return PaginatedData(page, pageSize, totalPages, pageData)
     }
 }
